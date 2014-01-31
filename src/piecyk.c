e35e9d885a1b8de15cdcc34bf8934ab6acca1529
Michał Sałaban <michal@salaban.info>
2014-02-01 00:39:34 +0100

    Use full 10-bit numbers for temperature settings

--- src/piecyk.c
+++ src/piecyk.c
@@ -18,25 +18,25 @@ FUSES =
 	 */
 };
 
-/* The sensor shows some kind of variarion, jumping even a full centigrade every
+/* The sensor shows some kind of variation, jumping even a full centigrade every
  * few seconds. For the heating we need very slow responsiveness, so the samples
  * will be gathered in a table and averaged.
  */
 #ifdef DEBUG
 #define		SAMPLE_BUFFER_SIZE	10
 #else
-#define		SAMPLE_BUFFER_SIZE	120		// remember this chip has 1K only
+#define		SAMPLE_BUFFER_SIZE	100		// remember this chip has 1K only
 #endif
 #define		INVALID_TEMP		-(300 << 2)	// mordor
 int16_t *samples;		// keep full 10-bit sample
 unsigned int samples_idx = 0;
 uint8_t buffer_filled = 0;
 
-/* WARNING: all temperatures are measured in 0,5C units, 9 bit */
+/* WARNING: all temperatures are measured in 0,25C units, 10 bit */
 #define		MIN_VALID_TEMP	0
-#define		MAX_VALID_TEMP	(50 << 1)
-#define		DEFAULT_MIN		(17 << 1) + 1
-#define		DEFAULT_MAX		(20 << 1) + 1
+#define		MAX_VALID_TEMP	(50 << 2)
+#define		DEFAULT_MIN		(22 << 2)		// 22,00
+#define		DEFAULT_MAX		(22 << 2) + 2	// 22,50
 uint16_t volatile temp_min = DEFAULT_MIN, temp_max = DEFAULT_MAX;
 uint8_t heating_state = 0;
 
@@ -90,7 +90,8 @@ volatile uint8_t display_need_refresh = 0;
 #define		MAX_VAL_IDX		0x0b
 
 const char template[DISPLAY_SIZE] PROGMEM	= "Temp   Max     C    C  Min     C";
-const char intro[DISPLAY_SIZE] PROGMEM		= "piecyk v0.3     zaraz grzejemy! ";
+const char intro[DISPLAY_SIZE] PROGMEM		= "piecyk v0.4     zaraz grzejemy! ";
+const char fractions[4] PROGMEM = "0358";
 
 inline void init_display()
 {
@@ -186,7 +187,6 @@ uint16_t get_avg_temp()
 {
 	unsigned int counted = SAMPLE_BUFFER_SIZE;
 	uint32_t sum = 0;
-	uint16_t avg;
 
 	buffer_filled = 1;
 	for (unsigned int i = 0; i < SAMPLE_BUFFER_SIZE; i++) {
@@ -197,11 +197,7 @@ uint16_t get_avg_temp()
 		}
 		sum += *(samples + i);
 	}
-	avg = sum / counted;
-	/* The samples are 10-bit integers in 0,25C units.
-	 * We use 9-bit integers in 0,5C units as they seem more user-friendly.
-	 */
-	return (avg >> 1) + (avg & 1);
+	return sum / counted;
 }
 
 #define		KEY_EDIT	(1 << PD4)
@@ -260,29 +256,32 @@ ISR(TIMER0_OVF_vect)
 
 void temptoa(uint16_t temp, char *rbuf)
 {
-	/* Converts 9-bit temp to string. Does not terminate the string with \0,
+	/* Converts 10-bit temp to string with one fraction digit.
+	 * The number gets rounded: ,25 = ,3 and ,75 = ,8
+	 *
+	 * Does not terminate the string with \0,
 	 * allowing simple pasting into the template. */
 	char *buf = malloc(4);
 	size_t s = 0;
 
-	itoa((temp >> 1), buf, 10);
+	// integer part: all but lowest 2 bits
+	itoa((temp >> 2), buf, 10);
 	s = strlen(buf);
 	memcpy(rbuf + (2-s), buf, s);
-	*(rbuf + s) = ',';
-	if (temp & 1) *(rbuf + s + 1) = '5';
-	else *(rbuf + s + 1) = '0';
+
+	*(rbuf + 2) = ',';
+	// fraction part: lowest 2 bits
+	*(rbuf + 3) = pgm_read_byte(&fractions[temp & 3]);
 	free(buf);
 }
 
 void refresh_display()
 {
 	// allocate once and for all
-	static char *display_buf = NULL, *buf = NULL;
+	static char *display_buf = NULL;
 	if (display_buf == NULL) display_buf = malloc(DISPLAY_SIZE + 1);
-	if (buf == NULL) buf = malloc(4);
 
 	memcpy_P(display_buf, &template, DISPLAY_SIZE);
-	*(display_buf + DISPLAY_SIZE) = '\0';
 	// Update values
 	if (buffer_filled)
 		*(display_buf + GATHER_IDX) =  ' ';
@@ -299,9 +298,10 @@ void refresh_display()
 		*(display_buf + MAX_EDIT_IDX) = '*';
 	else
 		*(display_buf + MAX_EDIT_IDX) = ' ';
-	display_need_refresh = 0;
+	*(display_buf + DISPLAY_SIZE) = '\0';
 	// Write the string to the display
 	hd44780fw_write(&lcd_conf, display_buf, 0, HD44780FW_WR_NO_CLEAR_BEFORE);
+	display_need_refresh = 0;
 }
 
 inline void init_keypad()
@@ -354,16 +354,17 @@ int main()
 	init_analog_temp();
 	init_keypad();
 	sei();
+	_delay_ms(3000);	// let the intro be visible but already gather data
 	for (unsigned int lcx = 0;; lcx++) {
-		_delay_ms(50);
-		if (!(lcx & 7)) {
-			// read temp and update heating state every 8 cycles
+		_delay_ms(31);		// ~32Hz
+		if (!(lcx & 0x1f)) {
+			// read temp and update heating state every 32 cycles
 			read_temp();
 			control_heating();
 		}
 
-		// refresh when needed or every 64 cycles
-		if (display_need_refresh || (lcx & 0x3f)) {
+		// refresh when needed or every 32 cycles
+		if (display_need_refresh || (lcx & 0x1f)) {
 			refresh_display();
 		}
 
